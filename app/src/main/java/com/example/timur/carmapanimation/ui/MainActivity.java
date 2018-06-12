7873fb44bfe11ab49362278199b882015688ceb0
ttashbaev <t.timurlan@gmail.com>
2018-06-12 19:26:43 +0600

    create sliding app panel

--- app/src/main/java/com/example/timur/carmapanimation/ui/MainActivity.java
+++ app/src/main/java/com/example/timur/carmapanimation/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.timur.carmapanimation;
+package com.example.timur.carmapanimation.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.timur.carmapanimation.R;
 
 public class MainActivity extends AppCompatActivity {
 
